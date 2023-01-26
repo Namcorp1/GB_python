5cd6786c7088ca981ed160f04060f8193e9e344d
namcorp1 <nikitamoskalev95@gmail.com>
2023-01-27 00:09:20 +0700

    add readme file and correct controller

--- homework_007/phonebook_MNA/controller.py
+++ homework_007/phonebook_MNA/controller.py
@@ -6,15 +6,17 @@ def phonebook():
     answer = input()
     if answer == 'да':
         database.import_data()
-    print('Сколько записей желаете добавить в справочник?')
-    count = int(input())
-    database.write_data(count) #аргумент функций - кол-во новых записей в телефонный справочник
+    print('Желаете внести новых абонентов в справочник (да/нет)?')
+    if answer == 'да':
+        print('Сколько записей желаете добавить в справочник?')
+        count = int(input())
+        database.write_data(count) #аргумент функций - кол-во новых записей в телефонный справочник
     print('В какой формат экспортировать данные, введите число (1 - сsv / 2 - txt / 3 - в оба формата)?')
     answer = input()
     if answer == '1':
         export.export_csv('export1.csv')
+    elif answer == '2':
+        export.export_txt('export.txt')
     elif answer == '3':
         export.export_csv('export1.csv')
-        export.export_txt('export.txt')
-    elif answer == '2':
         export.export_txt('export.txt')
